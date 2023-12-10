fbadcf24c75aae14727b38d6a73730a3409bcd19
kiran.gondi <kiran.gondi@planview.com>
2023-12-10 15:36:55 +0530

    Chapter 3 - JavaScript Object Properties

--- demo.js
+++ demo.js
@@ -1,8 +1,101 @@
 'use strict'; 
 (function() {
 
+//Chapter 3 - START
+
+    //Creating Property Getters & Setters
+    let person = {
+        name: {
+            firstName: 'Jim',
+            lastName: 'Cooper'
+        },
+        age: 29
+    };
+
+    Object.defineProperty(person, 'fullName',
+    {
+        get: function() {
+            return this.name.firstName + ' ' + this.name.lastName;
+        },
+        set: function(value){
+            var nameParts = value.split(' ');
+            this.name.firstName = nameParts[0];
+            this.name.lastName = nameParts[1];
+        }
+    });
+
+    //display(person.fullName);
+    person.fullName = 'Fred Jones';
+    display(person.fullName);
+    display(person.name.firstName);
+    display(person.name.lastName);
+
+    //Modifiying Properties with Property Descriptors
+    /*let person = {
+        firstName: 'Jim',
+        lastName: 'Cooper',
+        age: 29
+    };
+
+    Object.defineProperty(person, 'firstName', { configurable: false});
+    //Object.defineProperty(person, 'firstName', { enumerable: false});
+    //Object.defineProperty(person, 'firstName', { configurable: true}); --This is tricky one.
+    //Object.defineProperty(person, 'firstName', {writable: true});
+
+    delete person.firstName;
+    display(person);*/
+
+    /*refer above person object here too.
+
+    Object.defineProperty(person, 'firstName', {enumerable: false });
+
+    for(let propertyName in person) {
+        display(propertyName + ': ' + person[propertyName]);
+    }
+
+    display(Object.keys(person));
+    display(JSON.stringify(person));*/
+
+    /*let person = {
+        name: {
+            firstName: 'Jim',
+            lastName: 'Cooper'
+        },
+        age: 29
+    };
+
+    Object.defineProperty(person, 'name', {writable: false});
+
+    //Object.freeze(person.name);
+    person.name.firstName = 'Ted';
+
+    display(person.name);*/
+
+    // let person = {
+    //     firstName: 'Jim',
+    //     lastName: 'Cooper',
+    //     age: 29
+    // };
+
+    // Object.defineProperty(person, 'firstName', {writable: false});
+    // person.firstName = 'Ted';
+    //display(Object.getOwnPropertyDescriptor(person, "firstName"));
+
+    //Using Bracket Notation to Access JavaScript Properties
+    /* refer above person object here too.
+
+    // person['hair color'] = 'Brown';
+    // display(person['hair color']);
+    for(let propertyName in person){
+        display(propertyName + ': ' + person[propertyName]);
+    }*/
+
+
+//Chapter 3 - END
+
+    //Chapter 2 - START
     //Using Object.create()
-    let person1 = {
+    /*let person1 = {
         firstName: 'Jim',
         lastName: 'Cooper',
         age: 29
@@ -20,8 +113,8 @@
     display(person1);
     display(person2);
     //display(Object.is(person1, person2));
-    display(person1 === person1);
-    
+    display(person1 === person1);*/
+
     //Using Constructor Functions to Create Objects
     /*function Person(firstName, lastName, age){
         this.firstName = firstName;
@@ -89,19 +182,19 @@
 
     registerUser('Jim', 'Bhim');*/
 
-  /*let person = {
-    firstName: 'Jim',
-    lastName: 'Roland',
-    age: 16,
-    isAdult: function() { return this.age >= 18; }
-  };
-
-  //person.age = 22;
-
-  //display(person.age);
-  //display(person.lastName);
-  display(person.firstName);
-  display(person.isAdult());*/
+    /*let person = {
+      firstName: 'Jim',
+      lastName: 'Roland',
+      age: 16,
+      isAdult: function() { return this.age >= 18; }
+    };
   
+    //person.age = 22;
+  
+    //display(person.age);
+    //display(person.lastName);
+    display(person.firstName);
+    display(person.isAdult());*/
+    //Chapter 2 - END
   
 })();
